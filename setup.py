b3c589a2640f761616c34a5ee755d2e014239dcf
narancslee <204051657+narancslee@users.noreply.github.com>
2026-09-24 15:06:31 +0200

    Initialize ROS 2 Python package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+
+package_name = 'san_de9_autonom'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name],
+        ),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='narancslee',
+    maintainer_email='204051657+narancslee@users.noreply.github.com',
+    description='ROS 2 Python package for the autonomous robot assignment.',
+    license='GNU General Public License v3.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
